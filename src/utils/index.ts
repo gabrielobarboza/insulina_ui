a31dfda74a33ceb68ad138bd1678e387baaf5738
Gabriel Barboza <gabrielobarboza@gmail.com>
2022-01-18 15:25:33 -0300

    Logo

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,9 @@
 import enviroments from './enviroments'
 import tokenActions from './tokenActions'
-import serviceWorker from './serviceWorker'
 import validateUnits from './validateUnits'
 
 export {
     enviroments,
     tokenActions,
-    serviceWorker,
     validateUnits,
 }
