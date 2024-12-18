127aa93a5b87e1a03ea0c96b661260a130efacde
Gabriel Barboza <gabrielobarboza@gmail.com>
2024-12-18 17:43:06 -0300

    fixing build

--- config.ts
+++ config.ts
@@ -1,18 +1,6 @@
-import 'dotenv/config'
 
-const getEnvironmentVariable = (environmentVariable: string): string => {
-    const unvalidatedEnvironmentVariable = process.env[environmentVariable];
-    if (!unvalidatedEnvironmentVariable) {
-      throw new Error(
-        `Couldn't find environment variable: ${environmentVariable}`
-      );
-    } else {
-      return unvalidatedEnvironmentVariable;
-    }
-  };
-  
-  export const config = {
-    AppKey: getEnvironmentVariable('NEXT_PUBLIC_APP_KEY'),
-    ApiUrl: getEnvironmentVariable('NEXT_PUBLIC_API_URL'),
-    OAuthId: getEnvironmentVariable('NEXT_PUBLIC_OAUTH_ID'),
-  };
+export const config = {
+    AppKey: process.env.NEXT_PUBLIC_APP_KEY,
+    ApiUrl: process.env.NEXT_PUBLIC_API_URL,
+    OAuthId: process.env.NEXT_PUBLIC_OAUTH_ID,
+};
